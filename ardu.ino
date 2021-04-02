6252b15622cd8e22be42939d15e1f0292f5ec658
Julio Grisotto Alvarez <69219634+Julialva@users.noreply.github.com>
2021-04-01 21:01:07 -0300

    Add files via upload

---
+++ ardu.ino
@@ -0,0 +1,30 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);
+}
+void motor(){
+    if (Serial.available()){
+    byte ByteSerial=Serial.read();
+    if (ByteSerial='1'){
+      digitalWrite(8,LOW);
+      digitalWrite(7,HIGH);
+    }
+    if (ByteSerial == '2'){
+      digitalWrite(8,LOW);
+      digitalWrite(7,HIGH);
+    }
+    if (ByteSerial == '0') {
+      digitalWrite(8,LOW);
+      digitalWrite(7,LOW);
+    }
+  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ motor();
+ delay(15); // Espera de 15ms, para permitir que o servo atinj
+}
+
